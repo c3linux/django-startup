2951fde17370d25bbc1fbff29fa1b7bc66d206a1
celinux <c3linux@gmail.com>
2021-07-17 08:03:45 +0400

    article updated

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
+from blog.models.article import ArticleModel
 from django.contrib import admin
 from blog.models import CategoryModel
 # Register your models here.
 admin.site.register(CategoryModel)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'body')
+    list_display = (
+        'title', 'body', 'created_time', 'updated_time'
+    )
+
+
+admin.site.register(ArticleModel, ArticleAdmin)
--- blog/models/article.py
+++ blog/models/article.py
@@ -3,19 +3,24 @@ from django.db import models
 from autoslug import AutoSlugField
 from blog.models import CategoryModel
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
 
 class ArticleModel(models.Model):
-    image = models.ImageField(upload_to = 'article_images')
+    image = models.ImageField(upload_to='article_images')
     title = models.CharField(max_length=50)
-    body = models.TextField()
+    body = RichTextField()
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
-    slug = AutoSlugField(populate_from = 'body', unique = True)
+    slug = AutoSlugField(populate_from='body', unique=True)
     categories = models.ManyToManyField(CategoryModel, related_name='article')
-    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'articles')
+    writer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='articles')
 
     class Meta:
         db_table = 'article'
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
-        
+
+    def __str__(self):
+        return self.title
